28a105620cf632a421e0416ab0224ad40d6cf17d
luzuoquan <luzuoquanflex@gmail.com>
2014-07-17 22:24:00 +0800

    提交代码

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,5 +66,5 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['concat','uglify','watch']);
+	grunt.registerTask('default', ['concat','uglify','less','watch']);
 }
